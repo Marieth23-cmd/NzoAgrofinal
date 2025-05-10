17244cba72fe63a95ccb1836f6f226f9675a8f64
Marieth270 <134919554+Marieth270@users.noreply.github.com>
2025-05-10 23:45:43 +0100

    ainda nos produtos 2

--- src/app/Services/produtos.tsx
+++ src/app/Services/produtos.tsx
@@ -57,8 +57,8 @@ export interface Produto {
         {
           headers: {
             "Authorization": `Bearer ${token}`,
-            "Content-Type": "multipart/form-data"
-          }
+            },
+          withCredentials: true
         }
       );
   
