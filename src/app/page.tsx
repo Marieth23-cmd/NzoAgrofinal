1dee4f772855c20068f9157c29cb64ce463508a8
Marieth270 <134919554+Marieth270@users.noreply.github.com>
2025-05-18 20:29:45 +0100

    home 9

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -160,7 +160,7 @@ export default function Home() {
           </div>
         </section>
 
-          <section className="relative max-w-[300px] lg:max-w-[1000px] mb-72 lg:mb-20 mt-72 lg:mt-60">
+          <section className="relative max-w-[300px] lg:max-w-[1200px] ">
   <h2 className="text-center text-2xl font-bold mb-6">Produtos em Destaque</h2>
   {/* Só mostra setas se houver mais de 4 produtos em destaque */}
   {produtosDestaque.length > 4 && showRightButton && (
@@ -180,8 +180,8 @@ export default function Home() {
     {produtosDestaque.length > 0 ? (
       produtosDestaque.map((produto) => (
         <Link href={`/DetalhesProduto/${produto.id_produtos}`} key={produto.id_produtos}>
-          <div className="w-[100px] :h-[150px]  lg:w-[200px] lg:h-[250px] rounded-[10px] shadow-custom bg-white overflow-hidden flex-shrink-0">
-            <Image src={produto.foto_produto} alt={produto.nome} height={190} width={200} className="object-cover w-full h-[200px]" />
+          <div className="w-[300px] h-[350px] rounded-[10px] shadow-custom bg-white overflow-hidden flex-shrink-0 mb-[20%] lg:mb-28 mt-72 lg:mt-60">
+            <Image src={produto.foto_produto} alt={produto.nome} height={200} width={250} className="object-cover w-full h-[200px]" />
             <div className="p-4">
               <h3 className="text-[1.1rem] mb-2 font-bold">{produto.nome}</h3>
               <h3 className="text-[1.2rem] text-marieth font-bold">kzs {Number(produto.preco).toFixed(2)}/{produto.quantidade}{produto.Unidade}</h3>
