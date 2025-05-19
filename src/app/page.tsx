114241bf2a71e82c4a06e56cd85aa35ca1504f82
Marieth270 <134919554+Marieth270@users.noreply.github.com>
2025-05-19 01:33:30 +0100

    centralizando melhor o texto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -115,13 +115,15 @@ export default function Home() {
       <Navbar />
 
       {/* Ajustado margins para centralizar melhor */}
-      <div className="backgroundimage w-full flex flex-col items-center justify-center text-center mt-[20%] lg:mt-[15%] mb-20">
-        <h1 className="text-[1rem] lg:text-[2rem] font-bold">Conectando o Campo à sua Mesa</h1>
-        <p className="hidden lg:block">Encontre produtos frescos direto dos produtores locais</p>
-        <button onClick={redirecionamento} className="mt-1 bg-marieth text-white py-2 px-4 lg:mt-2 lg:py-4 lg:px-8 text-[1.1rem] border-none rounded-[5px] cursor-pointers hover:bg-verdeaceso transition-colors duration-150">
+      <div className="backgroundimage w-full flex flex-col items-center justify-center text-center mt-[20%] lg:mt-[15%]">
+        <h1 className="text-[1rem] lg:text-[2rem] font-bold text-center">Conectando o Campo à sua Mesa</h1>
+        <p className="hidden lg:block text-center ">Encontre produtos frescos direto dos produtores locais</p>
+        <button onClick={redirecionamento} className="items-center justify-center mt-1 bg-marieth text-white py-2 px-4 lg:mt-2 lg:py-4 
+        lg:px-8 text-[1.1rem] border-none rounded-[5px] cursor-pointers hover:bg-verdeaceso transition-colors duration-150">
           Começar a Comprar
         </button>
       </div>
+
       
       {/* Reduzido o margin-top para evitar espaçamento excessivo */}
       <div className="flex-grow flex flex-col items-center justify-start w-full px-4 sm:px-8 lg:px-16 gap-6 mt-12 lg:mt-16">
@@ -219,7 +221,7 @@ export default function Home() {
               )}
             </div>
             
-            {produtosDestaque.length > 4 && showLeftButton && (
+            {produtosDestaque.length > 3 && showLeftButton && (
               <button
                 onClick={() => scroll("left")}
                 className="hidden md:block absolute left-0 top-1/2 transform -translate-y-1/2 bg-white p-2 shadow-lg rounded-full z-10"
